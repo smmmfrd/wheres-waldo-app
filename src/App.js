f1a049b4fb8d29993eab6617a0d0de4b524d4ba9
Sam Mumford <smmmfrd@gmail.com>
2022-08-29 11:13:21 -0700

    Input Validation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import { useEffect, useRef } from "react";
+import data from "./data"
+
 import gc from "./assets/wheres-waldo-gc.png"
-import n64 from "./assets/wheres-waldo-n64.png"
-import wii from "./assets/wheres-waldo-wii.png"
+// import n64 from "./assets/wheres-waldo-n64.png"
+// import wii from "./assets/wheres-waldo-wii.png"
 
 function App() {
-  const currentImg = useRef();
+  const handleMouseDown = (event) => {
+    const inputX = Math.round((event.pageX / event.target.clientWidth) * 100)
+    const inputY = Math.round((event.pageY/event.target.clientHeight) * 100)
 
-  const handleMouseDown = (event) =>{
-    const x = Math.round((event.pageX / event.target.clientWidth) * 100)
-    const y = Math.round((event.pageY/event.target.clientHeight) * 100)
-    console.log(`Percentage Location: (${x}%, ${y}%)`);
+    const point = data.find((item) => {
+      return (inputX >= item.x && inputX <= item.w) && (inputY >= item.y && inputY <= item.h)
+    })
+
+    console.log(point ? point.character : "Nothing Found")
   }
-  
-  useEffect(() => {
-    window.addEventListener('click', handleMouseDown)
-    
-    return () => {
-      window.removeEventListener('click', handleMouseDown)
-    }
-  },[])
 
   return (
     <div className="App">
-      <img ref={currentImg} src={gc} alt="A where's waldo featuring characters from GameCube games." width="100%"/>
+      <img onClick={handleMouseDown} src={gc} alt="A where's waldo featuring characters from GameCube games." width="100%"/>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,25 @@
+const data = [
+    {
+        character: "Mario",
+        x: 38,
+        y: 69,
+        w: 44,
+        h: 74
+    },
+    {
+        character: "Boo",
+        x: 84,
+        y: 41,
+        w: 89,
+        h: 44
+    },
+    {
+        character: "Toad",
+        x: 33,
+        y: 38,
+        w: 36,
+        h: 40
+    }
+]
+
+export default data;
